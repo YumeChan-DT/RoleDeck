da8399af8f3abbbf40dcf35a9bb3f9af67cfa560
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2021-04-22 14:10:11 +0200

    Create PluginManifest

---
+++ PluginManifest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Nodsoft.YumeChan.PluginBase;
+
+namespace Nodsoft.YumeChan.RoleDeck
+{
+	public class PluginManifest : Plugin
+	{
+		private readonly ILogger<PluginManifest> logger;
+
+		public override string PluginDisplayName => "Yume-Chan RoleDeck";
+		public override bool PluginStealth => false;
+
+
+		public PluginManifest(ILogger<PluginManifest> logger)
+		{
+			this.logger = logger;
+		}
+
+
+		public override async Task LoadPlugin()
+		{
+			await base.LoadPlugin();
+
+			logger.LogInformation("Loaded {0}.", PluginDisplayName);
+		}
+
+		public override async Task UnloadPlugin() 
+		{
+			logger.LogInformation("Unloading {0}...", PluginDisplayName);
+
+			await base.UnloadPlugin();
+		}
+
+		public override IServiceCollection ConfigureServices(IServiceCollection services) => services;
+	}
+}
