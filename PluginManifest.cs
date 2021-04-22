39a688bf154f8ed38e9e5ba9491c08b80c252d8d
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2021-04-22 15:17:58 +0200

    Add UserReactionsListener

--- PluginManifest.cs
+++ PluginManifest.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DSharpPlus;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Logging;
 using Nodsoft.YumeChan.PluginBase;
 
@@ -11,21 +13,28 @@ namespace Nodsoft.YumeChan.RoleDeck
 {
 	public class PluginManifest : Plugin
 	{
-		private readonly ILogger<PluginManifest> logger;
-
 		public override string PluginDisplayName => "Yume-Chan RoleDeck";
 		public override bool PluginStealth => false;
 
 
-		public PluginManifest(ILogger<PluginManifest> logger)
+		private readonly ILogger<PluginManifest> logger;
+		private readonly DiscordClient discordClient;
+		private readonly UserReactionsListener reactionsListener;
+
+		public PluginManifest(ILogger<PluginManifest> logger, DiscordClient discordClient, UserReactionsListener reactionsListener)
 		{
 			this.logger = logger;
+			this.discordClient = discordClient;
+			this.reactionsListener = reactionsListener;
 		}
 
 
 		public override async Task LoadPlugin()
 		{
 			await base.LoadPlugin();
+
+			discordClient.MessageReactionAdded += reactionsListener.OnMessageReactionAddedAsync;
+			discordClient.MessageReactionRemoved += reactionsListener.OnMessageReactionRemovedAsync;
 
 			logger.LogInformation("Loaded {0}.", PluginDisplayName);
 		}
@@ -37,6 +46,7 @@ namespace Nodsoft.YumeChan.RoleDeck
 			await base.UnloadPlugin();
 		}
 
-		public override IServiceCollection ConfigureServices(IServiceCollection services) => services;
+		public override IServiceCollection ConfigureServices(IServiceCollection services) => services
+			.AddSingleton<UserReactionsListener>();
 	}
 }
---
+++ UserReactionsListener.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.EventArgs;
+using Microsoft.Extensions.Logging;
+
+namespace Nodsoft.YumeChan.RoleDeck
+{
+	public class UserReactionsListener
+	{
+		private readonly ILogger<UserReactionsListener> logger;
+
+		public UserReactionsListener(ILogger<UserReactionsListener> logger)
+		{
+			this.logger = logger;
+		}
+
+		public async Task OnMessageReactionAddedAsync(DiscordClient client, MessageReactionAddEventArgs e)
+		{
+			if ((await e.Channel.GetMessageAsync(e.Message.Id)).Author.IsCurrent)
+			{
+				logger.LogDebug("Message {0} : Added reaction {1} by {2}.", e.Message.Id, e.Emoji, e.User);
+			}
+		}
+
+		public async Task OnMessageReactionRemovedAsync(DiscordClient client, MessageReactionRemoveEventArgs e)
+		{
+			if ((await e.Channel.GetMessageAsync(e.Message.Id)).Author.IsCurrent)
+			{
+				logger.LogDebug("Message {0} : Removed reaction {1} by {2}.", e.Message.Id, e.Emoji, e.User);
+			}
+		}
+	}
+}
