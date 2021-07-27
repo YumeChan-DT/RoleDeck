79b06b01ecd42c1f607b02989d1e97f84ba8887d
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2021-07-27 09:28:15 +0200

    Draft Database Record for Initial Roles storage

---
+++ Data/InitialRoles.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+
+
+
+namespace YumeChan.RoleDeck.Data
+{
+	/// <summary>
+	/// Provides initial roles to apply to user, upon server join.
+	/// </summary>
+	public record InitialRoles
+	{
+		[BsonId, BsonRepresentation(BsonType.Int64)]
+		public ulong GuildId { get; set; }
+
+		public List<ulong> RoleIds { get; set; }
+	}
+}
--- Data/RoleMessage.cs
+++ Data/RoleMessage.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using DSharpPlus.Entities;
-using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
-using YumeChan.PluginBase.Tools.Data;
-using YumeChan.RoleDeck.Services;
 
 namespace YumeChan.RoleDeck.Data
 {
