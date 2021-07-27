2a09dd785f16b33c2e4cd3fb13116def55ba7617
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2021-07-27 10:18:19 +0200

    Add InitialRoles Service (w/ CRD operations)

---
+++ Services/InitialRolesService.cs
@@ -0,0 +1,62 @@
+﻿using DSharpPlus.Entities;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YumeChan.PluginBase.Tools.Data;
+using YumeChan.RoleDeck.Data;
+
+namespace YumeChan.RoleDeck.Services
+{
+	class InitialRolesService
+	{
+		private readonly IMongoCollection<InitialRoles> initialRoles;
+
+		public InitialRolesService(IDatabaseProvider<PluginManifest> databaseProvider)
+		{
+			 initialRoles = databaseProvider.GetMongoDatabase().GetCollection<InitialRoles>(nameof(InitialRoles));
+		}
+
+		public async Task<IEnumerable<ulong>> GetGuildRoles(ulong guildId) => (await initialRoles.FindAsync(ir => ir.GuildId == guildId)).FirstOrDefault()?.RoleIds;
+
+		public async Task AddGuildRole(ulong guildId, DiscordRole role)
+		{
+			InitialRoles roles = await FindOrInsertDbAsync(guildId);
+
+			if (!roles.RoleIds.Contains(role.Id))
+			{
+				await initialRoles.UpdateOneAsync(
+					Builders<InitialRoles>.Filter.Eq(ir => ir.GuildId, guildId),
+					Builders<InitialRoles>.Update.Push(ir => ir.RoleIds, role.Id)
+				);
+			}
+		}
+
+		public async Task RemoveGuildRole(ulong guildId, DiscordRole role)
+		{
+			InitialRoles roles = await FindOrInsertDbAsync(guildId);
+
+			if (roles.RoleIds.Contains(role.Id))
+			{
+				await initialRoles.UpdateOneAsync(
+					Builders<InitialRoles>.Filter.Eq(ir => ir.GuildId, guildId),
+					Builders<InitialRoles>.Update.Pull(ir => ir.RoleIds, role.Id)
+				);
+			}
+		}
+
+		public Task ClearGuildRoles(ulong guildId) => initialRoles.DeleteOneAsync(ir => ir.GuildId == guildId);
+
+
+		private async Task<InitialRoles> FindOrInsertDbAsync(ulong guildId)
+		{
+			if ((await initialRoles.FindAsync(ir => ir.GuildId == guildId)).FirstOrDefault() is not InitialRoles roles)
+			{
+				roles = new() { GuildId = guildId };
+				await initialRoles.InsertOneAsync(roles);
+			}
+
+			return roles;
+		}
+	}
+}
